a2e875c9f9192e913689fd7bb9de1138fc96ded4
gatino <gatinocorp@gmail.com>
2024-10-09 10:46:17 -0300

    ...

--- src/middlewares/token.ts
+++ src/middlewares/token.ts
@@ -7,12 +7,17 @@ import bcrypt from "bcrypt"
 export const createToken: RequestHandler = async (req, res) => {
   const db = await connect()
   const { email, password } = req.body
+
   const user = await db.get('SELECT id, email, password FROM users WHERE email = ? LIMIT 1', [email])
+
   if (!user)
     return res.status(401).json({ 'message': 'Invalid email' })
+  
   const match = await bcrypt.compare(password, user.password)
+
   if (!match)
     return res.status(401).json({ 'message': 'Invalid password' })
+  
   delete user.password
   const token = await sign(user)
   res.json({ token })
@@ -20,18 +25,23 @@ export const createToken: RequestHandler = async (req, res) => {
 
 export const verifyToken: RequestHandler = async (req, res, next) => {
   if (!req.headers.authorization)
-    return res.status(401).send('No token provided')
+    return res.status(401).json({ message: 'No token provided' })
+
   const token = req.headers.authorization
+
   try {
     await verify(token)
     next()
   } catch (err) {
     if (err instanceof jwt.TokenExpiredError)
       return res.status(401).json({ message: 'Token expired' })
+
     if (err instanceof jwt.NotBeforeError)
-      return res.status(401).json({ message: 'Token not active' })
+      return res.status(401).json({ message: 'Token not active yet' })
+
     if (err instanceof jwt.JsonWebTokenError)
       return res.status(401).json({ message: 'Invalid token' })
+
     res.status(401).send('Invalid token [n]')
   }
 }
@@ -45,5 +55,7 @@ export const refreshToken: RequestHandler = (req, res) => {
 }
 
 export default {
+  createToken,
+  verifyToken,
   refreshToken
 }
